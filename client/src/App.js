cd8487bf14e3f2dacb5504b30ec5ffd366413dd0
Dan Thackray <djthackray@gmail.com>
2022-08-18 15:32:13 +0100

    Small cleanups

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,6 @@ import React from "react";
 const App = () => {
   return (
     <div>
-        {/* <form className="form" action="" method="get">
-            <label className="label" for="name">Enter your name: </label>
-            <input className="field" type="text" name="name" id="name" required />
-            <label className="label" for="age">Enter your age: </label>
-            <input className="field" type="age" name="age" id="age" required />
-            <input className="button" type="submit" value="Submit!" />
-      </form> */}
       <Form />
     </div>)
 }
